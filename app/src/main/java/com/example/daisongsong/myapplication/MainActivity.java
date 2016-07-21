873e276de22d2d67eca4da9b1a7ff8fbc6d86a0b
daisongsong <gzdaisongsong@corp.netease.com>
2016-07-21 18:42:37 +0800

    add test

--- app/src/main/java/com/example/daisongsong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/daisongsong/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.daisongsong.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.netease.idate.net.t.Test;
@@ -11,13 +12,34 @@ import java.util.Locale;
 public class MainActivity extends Activity {
     private TextView mTextViewTest;
 
+    private TextView mTextViewContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextViewTest = (TextView) findViewById(R.id.mTextViewTest);
+        mTextViewContent = (TextView) findViewById(R.id.mTextViewContent);
 
         mTextViewTest.setText(String.format(Locale.CHINA, "10+20=%d", Test.add(10, 20)));
+
+        findViewById(R.id.mButtonNet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Test t = new Test();
+                t.testNet(new Test.NetCallback() {
+                    @Override
+                    public void onSuccess(String s) {
+                        mTextViewContent.setText(s);
+                    }
+
+                    @Override
+                    public void onFailure(String e) {
+                        mTextViewContent.setText(e);
+                    }
+                });
+            }
+        });
     }
 }
--- net/src/main/java/com/netease/idate/net/t/Test.java
+++ net/src/main/java/com/netease/idate/net/t/Test.java
@@ -1,12 +1,63 @@
 package com.netease.idate.net.t;
 
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by daisongsong on 16-7-21.
  */
 public class Test {
+    private static final String TAG = "Test";
 
     public static int add(int a, int b) {
         return a + b;
     }
 
+
+    public void testNet(final NetCallback callback) {
+        final Handler handler = new Handler();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://www.163.com")
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                Log.d(TAG, "onFailure() called with: " + "call = [" + call + "], e = [" + e + "]");
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onFailure(e.toString());
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String s = response.body().toString();
+                Log.d(TAG, "onResponse() called with: " + "call = [" + call + "], response = [" + response + "]");
+                Log.d(TAG, "response string is [" + s + "]");
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.onSuccess(s);
+                    }
+                });
+            }
+        });
+    }
+
+    public interface NetCallback {
+        void onSuccess(String s);
+
+        void onFailure(String e);
+    }
 }
