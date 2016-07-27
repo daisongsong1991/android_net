4e6dc4c901bf7f679afb770d0b0338357520359e
daisongsong <gzdaisongsong@corp.netease.com>
2016-07-27 15:49:46 +0800

    添加post的基本参数类型的处理

--- app/src/main/java/com/example/daisongsong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/daisongsong/myapplication/MainActivity.java
@@ -10,12 +10,16 @@ import com.netease.idate.net.okhttp.OkHttpNetClient;
 import com.netease.idate.net.t.Test;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends Activity {
     private TextView mTextViewTest;
 
     private TextView mTextViewContent;
+    private NetClient mNetClient = new OkHttpNetClient();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,46 @@ public class MainActivity extends Activity {
         mTextViewTest.setText(String.format(Locale.CHINA, "10+20=%d", Test.add(10, 20)));
 
         findViewById(R.id.mButtonNet).setOnClickListener(new View.OnClickListener() {
-            private NetClient mNetClient = new OkHttpNetClient();
 
             @Override
             public void onClick(View v) {
                 mNetClient.get("http://10.250.22.52:8080", null, new NetHandler() {
+                    @Override
+                    public void onResponse(int httpCode, final byte[] body) {
+                        mTextViewContent.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    mTextViewContent.setText(new String(body, "UTF-8"));
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(int httpCode, final String message) {
+                        mTextViewContent.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mTextViewContent.setText(message);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.mButtonTestPost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> params = new HashMap<>();
+                params.put("age", 19);
+                params.put("name", "netease");
+                params.put("isFemale", false);
+
+                mNetClient.post("http://10.250.22.52:8080/user/fillinfo", params, new NetHandler() {
                     @Override
                     public void onResponse(int httpCode, final byte[] body) {
                         mTextViewContent.post(new Runnable() {
--- net/src/main/java/com/netease/idate/net/okhttp/OkHttpNetClient.java
+++ net/src/main/java/com/netease/idate/net/okhttp/OkHttpNetClient.java
@@ -4,7 +4,9 @@ import com.netease.idate.net.api.NetClient;
 import com.netease.idate.net.api.NetHandler;
 import okhttp3.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -79,16 +81,48 @@ public class OkHttpNetClient extends NetClient {
             throw new RuntimeException("method POST must have Params");
         }
 
-        RequestBody body = null;
-        for (Map.Entry<String, Object> e : params.entrySet()) {
-
-        }
+        RequestBody body = createRequestBody(params);
 
         Request request = new Request.Builder()
                 .url(url)
                 .method("POST", body)
                 .build();
         return enqueueRequest(request, handler);
+    }
+
+    private RequestBody createRequestBody(Map<String, Object> params) {
+        boolean hasMultiData = false;
+        RequestBody body = null;
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        FormBody.Builder formBuilder = null;
+
+        for (Map.Entry<String, Object> e : params.entrySet()) {
+            Object value = e.getValue();
+            String key = e.getKey();
+            if (value instanceof File) {
+                // TODO: 16-7-27
+                hasMultiData = true;
+            } else if (value instanceof InputStream) {
+                // TODO: 16-7-27
+                hasMultiData = true;
+            } else {
+                if (formBuilder == null) {
+                    formBuilder = new FormBody.Builder();
+                }
+                formBuilder.add(key, String.valueOf(value));
+            }
+        }
+
+        if (formBuilder != null) {
+            if (hasMultiData) {
+                MultipartBody.Part part = MultipartBody.Part.create(formBuilder.build());
+                builder.addPart(part);
+                body = builder.build();
+            } else {
+                body = formBuilder.build();
+            }
+        }
+        return body;
     }
 
 
