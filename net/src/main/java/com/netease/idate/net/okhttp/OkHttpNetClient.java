d8d4f6380d9c079411141f636d5357cb5488e679
daisongsong <gzdaisongsong@corp.netease.com>
2016-08-05 17:24:24 +0800

    refactor Request Factory

--- net/src/main/java/com/netease/idate/net/okhttp/OkHttpNetClient.java
+++ net/src/main/java/com/netease/idate/net/okhttp/OkHttpNetClient.java
@@ -2,21 +2,29 @@ package com.netease.idate.net.okhttp;
 
 import com.netease.idate.net.api.NetClient;
 import com.netease.idate.net.api.NetHandler;
-import okhttp3.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by daisongsong on 16-7-25.
  */
 public class OkHttpNetClient extends NetClient {
     private OkHttpClient mOkHttpClient;
+
+    private RequestFactory mRequestFactory = RequestFactory.getInstance();
 
     public OkHttpNetClient() {
         mOkHttpClient = new OkHttpClient.Builder()
@@ -56,22 +64,7 @@ public class OkHttpNetClient extends NetClient {
 
     @Override
     public Object get(String url, Map<String, Object> params, NetHandler handler) {
-        String requestBody = null;
-        if (params != null) {
-            StringBuilder sb = new StringBuilder("?");
-            for (Map.Entry<String, Object> e : params.entrySet()) {
-                sb.append(String.format("%s=%s&", e.getKey(), String.valueOf(e.getValue())));
-            }
-            requestBody = sb.substring(0, sb.length() - 1);
-        }
-
-        String fullUrl = requestBody == null || requestBody.length() == 0 ? url : url + requestBody;
-
-        Request request = new Request.Builder()
-                .url(fullUrl)
-                .method("GET", null)
-                .build();
-
+        Request request = mRequestFactory.createGetRequest(url, params);
         return enqueueRequest(request, handler);
     }
 
@@ -81,50 +74,10 @@ public class OkHttpNetClient extends NetClient {
             throw new RuntimeException("method POST must have Params");
         }
 
-        RequestBody body = createRequestBody(params);
+        Request request = mRequestFactory.createPostRequest(url, params);
 
-        Request request = new Request.Builder()
-                .url(url)
-                .method("POST", body)
-                .build();
         return enqueueRequest(request, handler);
     }
-
-    private RequestBody createRequestBody(Map<String, Object> params) {
-        boolean hasMultiData = false;
-        RequestBody body = null;
-        MultipartBody.Builder builder = new MultipartBody.Builder();
-        FormBody.Builder formBuilder = null;
-
-        for (Map.Entry<String, Object> e : params.entrySet()) {
-            Object value = e.getValue();
-            String key = e.getKey();
-            if (value instanceof File) {
-                // TODO: 16-7-27
-                hasMultiData = true;
-            } else if (value instanceof InputStream) {
-                // TODO: 16-7-27
-                hasMultiData = true;
-            } else {
-                if (formBuilder == null) {
-                    formBuilder = new FormBody.Builder();
-                }
-                formBuilder.add(key, String.valueOf(value));
-            }
-        }
-
-        if (formBuilder != null) {
-            if (hasMultiData) {
-                MultipartBody.Part part = MultipartBody.Part.create(formBuilder.build());
-                builder.addPart(part);
-                body = builder.build();
-            } else {
-                body = formBuilder.build();
-            }
-        }
-        return body;
-    }
-
 
     private Call enqueueRequest(Request request, final NetHandler handler) {
         Call call = mOkHttpClient.newCall(request);
---
+++ net/src/main/java/com/netease/idate/net/okhttp/RequestFactory.java
@@ -0,0 +1,92 @@
+package com.netease.idate.net.okhttp;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by daisongsong on 16-8-5.
+ */
+public class RequestFactory {
+
+
+    private RequestFactory() {
+    }
+
+    public static final RequestFactory getInstance() {
+        return RequestFactoryInstance.INSTANCE;
+    }
+
+    public Request createGetRequest(String url, Map<String, Object> params) {
+        String requestBody = null;
+        if (params != null) {
+            StringBuilder sb = new StringBuilder("?");
+            for (Map.Entry<String, Object> e : params.entrySet()) {
+                sb.append(String.format("%s=%s&", e.getKey(), String.valueOf(e.getValue())));
+            }
+            requestBody = sb.substring(0, sb.length() - 1);
+        }
+
+        String fullUrl = requestBody == null || requestBody.length() == 0 ? url : url + requestBody;
+
+        Request request = new Request.Builder()
+                .url(fullUrl)
+                .method("GET", null)
+                .build();
+        return request;
+    }
+
+    public Request createPostRequest(String url, Map<String, Object> params) {
+        RequestBody body = createRequestBody(params);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .build();
+        return request;
+    }
+
+    private RequestBody createRequestBody(Map<String, Object> params) {
+        boolean hasMultiData = false;
+        RequestBody body = null;
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        FormBody.Builder formBuilder = null;
+
+        for (Map.Entry<String, Object> e : params.entrySet()) {
+            Object value = e.getValue();
+            String key = e.getKey();
+            if (value instanceof File) {
+                // TODO: 16-7-27
+                hasMultiData = true;
+            } else if (value instanceof InputStream) {
+                // TODO: 16-7-27
+                hasMultiData = true;
+            } else {
+                if (formBuilder == null) {
+                    formBuilder = new FormBody.Builder();
+                }
+                formBuilder.add(key, String.valueOf(value));
+            }
+        }
+
+        if (formBuilder != null) {
+            if (hasMultiData) {
+                MultipartBody.Part part = MultipartBody.Part.create(formBuilder.build());
+                builder.addPart(part);
+                body = builder.build();
+            } else {
+                body = formBuilder.build();
+            }
+        }
+        return body;
+    }
+
+    private static final class RequestFactoryInstance {
+        public static final RequestFactory INSTANCE = new RequestFactory();
+    }
+}
